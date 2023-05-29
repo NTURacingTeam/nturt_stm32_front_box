1016a6d1ff92873d2d7f3fc676ef76a12f9b094e
Tsou <astatine1184@gmail.com>
2023-05-29 14:29:11 +0800

    pedals add buffer structs

--- Core/Src/pedals.c
+++ Core/Src/pedals.c
@@ -34,8 +34,49 @@
 
 // freertos include
 #include "FreeRTOS.h"
-#include "task.h"
+// #include "task.h"
+#include "semphr.h"
 
 // project include
 #include "project_def.h"
 
+/**
+ * @brief structure to hold the data acquired by DMA
+ * 
+ */
+typedef struct{
+    uint16_t apps1;
+    uint16_t apps2;
+    uint16_t bse
+} adc_dma_buffer_t;
+static adc_dma_buffer_t adc_dma_buffer = {0} ;
+
+/**
+ * @brief structure to hold the data that is outputed by this function
+ * 
+ */
+typedef struct {
+    float apps1;
+    float apps2;
+    float bse;
+    uint8_t micro_apps;
+    uint8_t micro_bse;
+    SemaphoreHandle_t mutex
+} pedal_data_t;
+
+/**
+ * @brief handler function for the data acquisition task of the pedal sensors
+ * @note this function is to be executed periodically
+ * 
+ */
+void pedal_handler() {
+    
+    //TODO: set the conversion mode for the ADC to not blow up the buffers accidentally
+    HAL_ADC_Start_DMA(&hadc1, &(adc_dma_buffer.apps1), 1);
+    HAL_ADC_Start_DMA(&hadc3, &(adc_dma_buffer.apps2), 2);
+
+    uint8_t micro_apps = (uint8_t)HAL_GPIO_ReadPin(MICRO_APPS_PORT, MICRO_APPS_PIN);
+    uint8_t micro_bse = (uint8_t)HAL_GPIO_ReadPin(MICRO_BSE_PORT, MICRO_BSE_PORT);
+
+    return;
+}
